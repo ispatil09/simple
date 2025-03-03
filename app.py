040716c4af5c09b1a5940c9508fcaa8a85ff25b6
Ishwaragouda Patil <102734198+ispatil09@users.noreply.github.com>
2025-03-03 10:00:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index_new.html')
+    return 'index_new.html :: Flask app this is ...'
     #return "New"
 
 if __name__ == '__main__':
